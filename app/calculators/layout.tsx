39510c161801d7badea347c39c0f7ab903dbe882
geniuspark00 <geniuspark00-source@gmail.com>
2025-11-24 21:00:14 +0900

    auto push

--- app/calculators/layout.tsx
+++ app/calculators/layout.tsx
@@ -1,4 +1,4 @@
-import "../../globals.css";
+import "../../../globals.css";
 import Script from "next/script";
 
 export default function CalculatorsLayout({ children }: { children: React.ReactNode }) {
